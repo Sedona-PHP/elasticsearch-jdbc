d9366751d3a5d84cde77954a180548f338858dcc
tcucchietti <thomas.cucchietti@gmail.com>
2015-12-14 15:29:23 +0100

    Backport of JSON nested documents parsing
    Quick and dirty extraction of the code needed to parse documents in JSON (from jprante/elasticsearch-jdbc#518).
    Needed code has been included in ElasticSearch v2.0.1 (AbstractContentXParser, see elastic/elasticsearch#10455).

--- src/main/java/org/xbib/elasticsearch/common/util/PlainKeyValueStreamListener.java
+++ src/main/java/org/xbib/elasticsearch/common/util/PlainKeyValueStreamListener.java
@@ -19,15 +19,12 @@ import com.spatial4j.core.context.SpatialContext;
 import com.spatial4j.core.context.jts.JtsSpatialContext;
 import com.spatial4j.core.shape.Shape;
 import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentParser;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.xbib.elasticsearch.common.keyvalue.KeyValueStreamListener;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -148,7 +145,7 @@ public class PlainKeyValueStreamListener<K, V> implements KeyValueStreamListener
         }
         // create current object from values by sequentially merging the values
         for (int i = 0; i < keys.size() && i < values.size(); i++) {
-            Map map = null;
+			Object v = null;
             try {
                 String s = values.get(i).toString();
                 if (s.startsWith("POLYGON(") || s.startsWith("POINT(")) {
@@ -161,15 +158,95 @@ public class PlainKeyValueStreamListener<K, V> implements KeyValueStreamListener
                     s = builder.string();
                 }
                 // JSON content?
-                map = JsonXContent.jsonXContent.createParser(s).mapAndClose();
+
+				XContentParser parser = JsonXContent.jsonXContent.createParser(s);
+				XContentParser.Token token = parser.currentToken();
+				if(token == null) {
+					token = parser.nextToken();
+				}
+				if (token == XContentParser.Token.START_OBJECT) {
+					v = parser.map();
+				} else if (token == XContentParser.Token.START_ARRAY) {
+					v = readList(parser, SIMPLE_MAP_FACTORY);
+				}
             } catch (Exception e) {
                 // ignore
             }
-            Object v = map != null && map.size() > 0 ? map : values.get(i);
+			if(v == null || (v instanceof Map && ((Map) v).isEmpty())) {
+                v = values.get(i);
+            }
             Map<String, Object> m = merge(current.source(), keys.get(i), v);
             current.source(m);
         }
         return this;
+    }
+
+	static interface MapFactory {
+        Map<String, Object> newMap();
+    }
+
+    static final MapFactory SIMPLE_MAP_FACTORY = new MapFactory() {
+        @Override
+        public Map<String, Object> newMap() {
+            return new HashMap<>();
+        }
+    };
+
+	static List<Object> readList(XContentParser parser, MapFactory mapFactory) throws IOException {
+        ArrayList<Object> list = new ArrayList<>();
+        XContentParser.Token token;
+        while ((token = parser.nextToken()) != XContentParser.Token.END_ARRAY) {
+            list.add(readValue(parser, mapFactory, token));
+        }
+        return list;
+    }
+
+	static Map<String, Object> readMap(XContentParser parser, MapFactory mapFactory) throws IOException {
+        Map<String, Object> map = mapFactory.newMap();
+        XContentParser.Token token = parser.currentToken();
+        if (token == null) {
+            token = parser.nextToken();
+        }
+        if (token == XContentParser.Token.START_OBJECT) {
+            token = parser.nextToken();
+        }
+        for (; token == XContentParser.Token.FIELD_NAME; token = parser.nextToken()) {
+            // Must point to field name
+            String fieldName = parser.currentName();
+            // And then the value...
+            token = parser.nextToken();
+            Object value = readValue(parser, mapFactory, token);
+            map.put(fieldName, value);
+        }
+        return map;
+    }
+
+	static Object readValue(XContentParser parser, MapFactory mapFactory, XContentParser.Token token) throws IOException {
+        if (token == XContentParser.Token.VALUE_NULL) {
+            return null;
+        } else if (token == XContentParser.Token.VALUE_STRING) {
+            return parser.text();
+        } else if (token == XContentParser.Token.VALUE_NUMBER) {
+            XContentParser.NumberType numberType = parser.numberType();
+            if (numberType == XContentParser.NumberType.INT) {
+                return parser.intValue();
+            } else if (numberType == XContentParser.NumberType.LONG) {
+                return parser.longValue();
+            } else if (numberType == XContentParser.NumberType.FLOAT) {
+                return parser.floatValue();
+            } else if (numberType == XContentParser.NumberType.DOUBLE) {
+                return parser.doubleValue();
+            }
+        } else if (token == XContentParser.Token.VALUE_BOOLEAN) {
+            return parser.booleanValue();
+        } else if (token == XContentParser.Token.START_OBJECT) {
+            return readMap(parser, mapFactory);
+        } else if (token == XContentParser.Token.START_ARRAY) {
+            return readList(parser, mapFactory);
+        } else if (token == XContentParser.Token.VALUE_EMBEDDED_OBJECT) {
+            return parser.binaryValue();
+        }
+        return null;
     }
 
     protected void map(K k, V v, IndexableObject current) throws IOException {
